5d9c2c8b3fa34f0057792f2e3a7874f7cf43fddb
John Blank <veloblank@gmail.com>
2020-01-13 04:06:48 -0500

    Add search controller w/ CRUD and model

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::SearchesController < ApplicationController
+  def index
+    searches = Search.all
+    render json: searches, status: 200
+  end
+  
+  def create
+    search = Search.create(search_params)
+    render json: search, status: 200
+  end
+
+  def destroy
+    search = Search.find(params[:id])
+    search.delete
+    render json: {searchId: search.id}
+  end
+
+  # def show
+  #   search = Search.find(params[:id])
+  #   render json: search, status: 200
+  # end
+
+  private 
+
+  def search_params
+    params.require(:search).permit(:text)
+  end
+end
